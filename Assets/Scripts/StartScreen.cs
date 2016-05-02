a67e2cd210bfd26274d1051de2c11ab0ed41c718
jdavis4 <Jasmine Davis>
2016-05-02 22:16:12 +0200

    Update as of 5/2 10:16PM
    - Removed all extra and unused files from downloaded content
    - Added start scene as intro to game
    - Added Ceilings to maze and start screen
    - Created game logo (3 versions)
    - Made playable mummy a prefab
    - Updated health bar to work with ceilings

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	private Animator anim; // Reference to the animator component.
+	public string nextScene = "TestScene";
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+		anim.SetBool("Stand",true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Application.LoadLevel (nextScene);
+		}
+	}
+
+}
