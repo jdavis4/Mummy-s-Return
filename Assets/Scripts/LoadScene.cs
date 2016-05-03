6b74a0be474164f67e924f58376b7895e1ebe09b
jdavis4 <Jasmine Davis>
2016-05-03 23:34:59 +0200

    5/13/16 Update
    - added transition scene for any storyline/instructions
    - added scroll item that rotates and can be picked up
    - Small adjustments to start/landing screen
    - cleaned up some code files
    - Made some objects prefabs
    - Made sprites for parchment/scroll and LoadScene.scene background

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public string nextScene = "TestScene";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel(nextScene);
+    }
+}
